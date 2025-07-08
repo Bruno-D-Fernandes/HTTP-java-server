2992b5d2e543f93fb77654e260bb1a4460c40340
Bruno Fernandes <BrunoFernandes2315@Outlook.com>
2025-07-07 23:43:05 -0300

    Simulando um servidorzinho apache

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -2,23 +2,42 @@ import com.sun.net.httpserver.*;
 
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        server.createContext("/", exchange -> {
-            String resposta = "Funcionando!";
-            exchange.sendResponseHeaders(200, resposta.length());
-            exchange.getResponseBody().write(resposta.getBytes());
 
-            exchange.close();
-        });
+        server.createContext("/", exchange -> entregaArquivo(exchange, "index.html"));
 
         server.setExecutor(null);
         server.start();
 
+    }
+
+    static void entregaArquivo(HttpExchange exchange, String nome) throws IOException {
+        File arquivo = new File("www/" + nome);
+        if (!arquivo.exists()) {
+            System.out.println("Caiu no erro");
+
+            String resposta = "<h1>Arquivo não encontrado</h1>";
+            byte[] corpo = resposta.getBytes("UTF-8");
+
+            exchange.getResponseHeaders().set("Content-Type", "text/html; charset=utf-8");
+            exchange.sendResponseHeaders(404, corpo.length);
+            exchange.getResponseBody().write(corpo);
+
+            exchange.close();
+            return;
+        }
+
+
+        exchange.getResponseHeaders().set("Content-Type", "text/html; charset=utf-8");
+        byte[] conteudo = Files.readAllBytes(arquivo.toPath());
+
+        exchange.sendResponseHeaders(200, conteudo.length);
+        exchange.getResponseBody().write(conteudo);
+        exchange.close();
     }
 }
